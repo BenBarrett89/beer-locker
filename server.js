3493c54b9472080c40e167818e22bb47a9262411
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2016-11-04 10:46:15 +0000

    Create our first model - BEER

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 // Get the packages we need
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require('express')
+var mongoose = require('mongoose')
+var Beer = require('./models/beer');
 
 // Connect to the beerlocker MongoDB
-mongoose.connect('mongodb://localhost:27017/beerlocker');
+mongoose.connect('mongodb://localhost:27017/beerlocker')
 
 // Create our Express application
-var app = express();
+var app = express()
 
 // Use environment defined port or 3000
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 
 // Create our Express router
-var router = express.Router();
+var router = express.Router()
 
 // Initial dummy route for testing
 // http://localhost:3000/api
 router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
-});
+  res.json({ message: 'You are running dangerously low on beer!' })
+})
 
 // Register all our routes with /api
-app.use('/api', router);
+app.use('/api', router)
 
 // Start the server
 app.listen(port);
-console.log('Insert beer on port ' + port);
+console.log('Insert beer on port ' + port)
