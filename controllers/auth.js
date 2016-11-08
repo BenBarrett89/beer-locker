8db906b98af30c85485cc0dfd11ac3a79f0312aa
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2016-11-08 15:16:46 +0000

    Access token authorization on API endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,6 +69,6 @@ passport.use(new BearerStrategy(
 ))
 
 // export as functions
-exports.isAuthenticated = passport.authenticate('basic', { session : false })
+exports.isAuthenticated = passport.authenticate(['basic','bearer'], { session : false })
 exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false })
 exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false })
