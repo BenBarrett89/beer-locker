d95e33327a2166836c472e693a584939dddc4a29
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2016-11-08 11:45:27 +0000

    Authenticate our application client

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 // Load required packages
 var passport = require('passport')
 var BasicStrategy = require('passport-http').BasicStrategy
+
+// Load the models
 var User = require('../models/user')
+var Client = require('../models/client');
 
 passport.use(new BasicStrategy(
   function(username, password, callback) {
@@ -25,4 +28,20 @@ passport.use(new BasicStrategy(
   }
 ))
 
+passport.use('client-basic', new BasicStrategy(
+  function(username, password, callback) {
+    Client.findOne({ id: username }, function (err, client) {
+      if (err) { return callback(err) }
+
+      // No client found with that id or bad password
+      if (!client || client.secret !== password) { return callback(null, false) }
+
+      // Success
+      return callback(null, client)
+    })
+  }
+))
+
+// export the functions
 exports.isAuthenticated = passport.authenticate('basic', { session : false })
+exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false })
